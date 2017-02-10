b79e07bbdb5ee1867464ff2ccd9b77cf05c275dc
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2017-02-10 17:24:22 +0100

    Added drupal container for drush.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # Build docker image(s) to image folder
-docker build -t cargo:nginx docker/cargo-nginx/
-docker build -t cargo:php docker/cargo-php/
+docker build -t cargo:nginx     docker/cargo-nginx/
+docker build -t cargo:php       docker/cargo-php/
+docker build -t cargo:drupal    docker/cargo-drupal/
