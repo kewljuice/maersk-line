75ce2541619d0bfbb0c2ca48b910ca971a226fd2
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2017-02-11 18:18:45 +0100

    Removed cargo:nginx and added application container.

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 # Build docker image(s) to image folder
-docker build -t cargo:nginx     docker/cargo-nginx/
 docker build -t cargo:php       docker/cargo-php/
 docker build -t cargo:drupal    docker/cargo-drupal/
