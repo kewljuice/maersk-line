e162d7882aac8d0ec14c5192fdfa5ee9729a0359
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2017-11-17 14:47:41 +0100

    Added limits and integrated build images in docker-compose.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Build docker image(s) to image folder
-docker build -t cargo:php       docker/cargo-php/
-docker build -t cargo:drupal    docker/cargo-drupal/
+#docker build -t cargo:php       docker/cargo-php/
+#docker build -t cargo:drupal    docker/cargo-drupal/
