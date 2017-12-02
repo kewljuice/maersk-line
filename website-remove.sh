d0925f4f3c4b3b4861014ecb897efd571ed67af1
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2017-12-02 14:16:15 +0100

    refs #develop added docker-compose stop.

--- website-remove.sh
+++ website-remove.sh
@@ -2,3 +2,10 @@
 
 # Run cleanup via cleanup.sh
 docker-compose run --rm build bash /var/www/cleanup.sh
+
+# TODO: remove complete mysql.
+# docker-compose run --rm mysql rm -rf /var/lib/mysql
+
+# docker-compose stop
+docker-compose stop
+docker-compose rm -f
